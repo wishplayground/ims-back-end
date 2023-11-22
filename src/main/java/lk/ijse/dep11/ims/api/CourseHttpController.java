1dd21a4082bd886811b10b069eb8a973c4ae6a22
Dakshitha Hasindra <hasindradh@gmail.com>
2023-11-22 14:47:27 +0530

    "Add Course Controller"

---
+++ src/main/java/lk/ijse/dep11/ims/api/CourseHttpController.java
@@ -0,0 +1,11 @@
+package lk.ijse.dep11.ims.api;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/courses")
+@CrossOrigin
+public class CourseHttpController {
+}
