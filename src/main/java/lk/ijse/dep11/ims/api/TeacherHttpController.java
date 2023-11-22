9ab6e7034a9b100cee0574bf4584578c4c093ca4
Wishva98 <buddhiwishvanath123@gmail.com>
2023-11-22 15:03:40 +0530

    create schema.sql

--- src/main/java/lk/ijse/dep11/ims/api/TeacherHttpController.java
+++ src/main/java/lk/ijse/dep11/ims/api/TeacherHttpController.java
@@ -1,4 +1,12 @@
 package lk.ijse.dep11.ims.api;
 
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/teachers")
 public class TeacherHttpController {
 }
